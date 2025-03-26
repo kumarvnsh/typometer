dfec0944822d827ab4a42a5a1f691011b734cf33
Vnsh Kumar <161806084+gamedevteam321@users.noreply.github.com>
2025-03-27 03:36:16 +0530

    Remove react-query and fix Sonner import

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
+function App() {
+  return (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <Router basename="/typometer">
+      <Router>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
@@ -21,7 +18,7 @@ const App = () => (
         </Routes>
       </Router>
     </TooltipProvider>
-  </QueryClientProvider>
-);
+  );
+}
 
 export default App;
